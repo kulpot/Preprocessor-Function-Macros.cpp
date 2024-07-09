88b744f80fa2420c05a41c64dddd1cc524f6fb5f
Kulpot <kulpotwahu@gmail.com>
2024-07-09 14:24:46 +0800

    ..

--- Preprocessor Function Macros/CppScratchPad.cpp
+++ Preprocessor Function Macros/CppScratchPad.cpp
@@ -5,10 +5,11 @@ using std::endl;
 
 //Preprocessor Function Macros   //ref link:https://www.youtube.com/watch?v=Cy10W_Ew_ys&list=PLRwVmtr-pp06gII17UNrzeCGMMHnCPdOk&index=2
 
-#define p(a) cout << a << endl	//macros - are compile time
+//#define p(a) cout << a << endl	//macros - are compile time
+#define PRINT(a) cout << a << endl		//macros typical form is in ALLCAPS
 
 void main()
 {
 	cout << 5 << endl;
-	p(5);	//p short for print
+	//p(5);	//p short for print
 }
